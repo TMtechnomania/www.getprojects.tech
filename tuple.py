17fa6abbf19da29e385474c1bd572e3744a01672
TMtechnomania <tiwari.kartik3002@gmail.com>
2022-04-21 19:40:14 +0530

    a bit of updates

--- tuple.py
+++ tuple.py
@@ -1,5 +1,11 @@
-tup = (1,2,3,4,5,6,7,8,9,0)
-sum = 0
-for i in tup:
-    sum = sum+i
-print("The sum is", sum)
+# tup = (1,2,3,4,5,6,7,8,9,0)
+# sum = 0
+# for i in tup:
+#     sum = sum+i
+# print("The sum is", sum)
+
+l1 = [1,2,3,4,5,6,7,8,9,0]
+l2 = l1
+print(l2)
+l2[0] = l2[0] + 10
+print(l2)
